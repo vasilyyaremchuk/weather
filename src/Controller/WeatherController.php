2a52ba2cb087c71a97b04c7ee109f9c334c40d82
Vasyl Yaremchuk <vasyl.yaremchuk@ffwagency.com>
2025-03-18 11:04:27 +0200

    Improve translations and routes.

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class WeatherController extends AbstractController
 {
@@ -20,36 +21,35 @@ class WeatherController extends AbstractController
         $this->parameterBag = $parameterBag;
     }
 
-    #[Route('/{_locale}/weather', name: 'weather_index', requirements: ['_locale' => 'en|uk'], defaults: ['_locale' => null])]
+    #[Route('/', name: 'root_redirect')]
+    public function rootRedirect(Request $request): Response
+    {
+        $defaultLocale = $this->parameterBag->get('app.locale');
+        return $this->redirectToRoute('weather_index', ['_locale' => $defaultLocale]);
+    }
+
+    #[Route('/{_locale}', name: 'weather_index', methods: ['GET', 'POST'], requirements: ['_locale' => 'en|uk'])]
     public function index(Request $request): Response
     {
         // Set locale from request or .env
         $locale = $request->attributes->get('_locale') ?? $this->parameterBag->get('app.locale');
         $request->setLocale($locale);
         
-        return $this->render('weather/index.html.twig', [
-            'city' => null,
-            'weatherData' => null,
-        ]);
-    }
-
-    #[Route('/{_locale}/weather/get', name: 'weather_get', methods: ['POST'], requirements: ['_locale' => 'en|uk'], defaults: ['_locale' => null])]
-    public function getWeather(Request $request): Response
-    {
-        // Set locale from request or .env
-        $locale = $request->attributes->get('_locale') ?? $this->parameterBag->get('app.locale');
-        $request->setLocale($locale);
+        $city = null;
+        $weatherData = null;
         
-        $city = $request->request->get('city', 'London');
-        $weatherData = $this->weatherService->getWeatherData($city);
-
+        if ($request->isMethod('POST')) {
+            $city = $request->request->get('city', 'London');
+            $weatherData = $this->weatherService->getWeatherData($city);
+        }
+        
         return $this->render('weather/index.html.twig', [
             'city' => $city,
             'weatherData' => $weatherData,
         ]);
     }
 
-    #[Route('/{_locale}/weather/api/{city}', name: 'weather_api', methods: ['GET'], requirements: ['_locale' => 'en|uk'], defaults: ['_locale' => null])]
+    #[Route('/{_locale}/api/{city}', name: 'weather_api', methods: ['GET'], requirements: ['_locale' => 'en|uk'])]
     public function getWeatherApi(Request $request, string $city): Response
     {
         // Set locale from request or .env
